9fe3a5f1fcd0b09d423c059dda0a9f4822cfd669
PJKUNG <poowadetthongsri558899@gmail.com>
2026-05-12 19:27:07 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("PhuPha Top Up Store Loaded");
+
+const startBtn = document.getElementById("startBtn");
+
+startBtn.addEventListener("click", () => {
+  alert("ระบบเริ่มใช้บริการกำลังพัฒนา");
+});
