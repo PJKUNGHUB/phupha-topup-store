f8aec2219044d6c990ab8c1ea79b628bee18e69b
PJKUNG <poowadetthongsri558899@gmail.com>
2026-05-13 08:56:27 +0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,33 @@
+const ui = {
 
+  openBottomSheet() {
+    document
+      .getElementById('sheetOverlay')
+      .classList.add('active');
+
+    document
+      .getElementById('orderSheet')
+      .classList.add('active');
+  },
+
+  closeBottomSheet() {
+    document
+      .getElementById('sheetOverlay')
+      .classList.remove('active');
+
+    document
+      .getElementById('orderSheet')
+      .classList.remove('active');
+  }
+
+};
+
+window.ui = ui;
+
+const overlay = document.getElementById('sheetOverlay');
+
+if (overlay) {
+  overlay.addEventListener('click', () => {
+    ui.closeBottomSheet();
+  });
+}
